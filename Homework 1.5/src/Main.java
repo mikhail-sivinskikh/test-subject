a57222db8964840a06a5bad804c33591a99e5738
mikhail-sivinskikh <45894940+mikhail-sivinskikh@users.noreply.github.com>
2018-12-15 12:58:36 +0200

    test

--- Homework 1.5/src/Main.java
+++ Homework 1.5/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Проверим заполнение по умолчанию: ");
+        System.out.println();
         Employee ivan = new Employee();
         ivan.getEmployee();
         System.out.println();
